fcabda77c30d2231ed28c410c5d2e6bc42581970
Danielle Marie <dhutchings36@yahoo.com>
2020-02-16 12:31:07 -0600

    working on deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: "http://localhost:8000/api",
+    // API_ENDPOINT: "http://localhost:8000/api",
+    API_ENDPOINT: "https://mighty-savannah-32180.herokuapp.com/api",
     TOKEN_KEY: "reviewstream-client-auth-token",
     USER_ID: "reviewstream-user-id"
 }
